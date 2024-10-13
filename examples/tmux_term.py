42fba7edb094dfab8b0bb3e531924b748344bb69
Art O Cathain <art.home@gmail.com>
2024-10-13 19:08:03 +0200

    ensure started

--- examples/tmux_term.py
+++ examples/tmux_term.py
@@ -1,3 +1,4 @@
+import asyncio
 from logging import getLogger
 from typing import List
 
@@ -11,7 +12,18 @@ from inspect_ai.util import sandbox
 
 logger = getLogger(__name__)
 
+# TODO: write PS1="\a$PS1" into the shell to start,
+# then use  tmux list-windows -F '#{window_bell_flag}:#{window_name}' | grep '^1'
+# but this will require hackery for the *current* window to still be flagged
+# Look at https://github.com/tmux/tmux/issues/2264#issuecomment-641112735 for the alert-bell hook
+# OR always do stuff in not the current window?
+# OR use https://github.com/rickstaa/tmux-notify ?
+# in fact, window_activity_flag looks maybe even better?
+# plan: wait for window_activity
+# then wait using monitor-silence?
 
+
+# TODO: FileNotFoundError for local sandbox exec of nonexistent command?
 @tool
 def term_send_text() -> Tool:
     async def execute(text: List[str]) -> bool:
@@ -39,11 +51,24 @@ def term_read() -> Tool:
         Returns:
             Contents of the terminal window.
         """
+        await _ensure_tmux_started()
         res = await sandbox().exec(["tmux", "capture-pane", "-p"])
 
         return res.stdout
 
     return execute
+
+
+async def _ensure_tmux_started():
+    problem_starting = True
+    try:
+        check_tmux = await sandbox().exec(["tmux", "-V"])  # just check the version
+        if check_tmux.returncode == 0:
+            problem_starting = False
+    except (PermissionError, FileNotFoundError) as ex:
+        logger.debug(f"Permission error starting tmux: {ex=}")
+    if problem_starting:
+        raise Exception("Could not execute tmux in the sandbox, is it installed?")
 
 
 @task
